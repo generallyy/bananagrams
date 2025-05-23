d373bea69f5eba229b5e96f2e855137da9d49865
aedan w. <aedan.wen@gmail.com>
2025-05-22 20:23:56 -0400

    start
    enabled multiplayer functionality. used a tutorial that uses movement- things are slightly difficult but i'm starting to understand

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var info_label = $Label
+
+func _ready():
+	update_info()
+
+func update_info():
+	if is_multiplayer_authority():
+		var player_id = multiplayer.get_unique_id()
+		var role = "Host" if multiplayer.is_server() else "Client"
+		info_label.text = "Player ID: %d\nRole: %s" % [player_id, role]
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
+func _physics_process(_delta):
+	if is_multiplayer_authority():
+		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 400
+
+	move_and_slide()
+
+
+	
